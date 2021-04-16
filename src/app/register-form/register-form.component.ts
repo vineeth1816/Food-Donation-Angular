575a2bed51f3fc2906f381fbbe213019a3654dfe
vineeth1816 <vineeth1816@gmail.com>
2021-04-16 15:39:12 +0530

    Register form validation completed

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterFormComponent implements OnInit {
 
+  model: any = {};
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validate(myForm:NgForm){
+    console.log(myForm);
+    if(myForm.form.invalid)
+    alert("Please update the highlighted madatory field(s).");
+    else
+    alert("New user created Successfully");
   }
 
 }
