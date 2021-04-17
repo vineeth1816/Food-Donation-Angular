62eb25652b0a1f07056e591bf838994274005905
VENKATNIKHIL-98 <munaganurivenkatnikhil@gmail.com>
2021-04-17 20:02:50 +0530

    forgotuserid

--- src/app/forgot-user-id/forgot-user-id.component.ts
+++ src/app/forgot-user-id/forgot-user-id.component.ts
@@ -1,4 +1,7 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ForgotuseridServiceService } from '../forgotuserid-service.service';
+import { User } from '../User';
 
 
 @Component({
@@ -8,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotUserIdComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public forgotuseridService:ForgotuseridServiceService) { }
+fu= new User()
+uidFlag:boolean=true;
+uid:string="";
+uidMessage:boolean=false;
+invalidIdFlag:boolean=false;
   ngOnInit(): void {
   }
 
+check(){
+  console.log(this.fu.contactNo,this.fu.emailId,this.fu.sq1,this.fu.sq2,this.fu.sq3)
+  this.forgotuseridService.getUser(this.fu).subscribe(result=>{
+    console.log(result);
+    if(result.userId!=null){
+      this.uidFlag=false;
+      this.uidMessage=true;
+      this.uid="Your user id is "+result.userId;
+
+    }
+    else{
+      alert("invalid credentials")
+      this.invalidIdFlag=true;
+      this.fu.contactNo="";
+      this.fu.emailId="";
+      this.fu.sq2="";
+      this.fu.sq1="";
+      this.fu.sq3="";
+
+    }
+  },
+  error=>console.log(error))
+}
 }
---
+++ src/app/forgotuserid-service.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForgotuseridServiceService {
+
+ 
+  constructor(public httpClient:HttpClient) { }
+
+  getUser(user:User):Observable<User>{
+    return this.httpClient.post<User>('http://localhost:8080/forgotuserid',
+    {firstName:user.firstName,lastName:user.lastName,dob:user.dob,gender:user.gender,emailId:user.emailId,contactNo:user.contactNo,userCategory:user.userCategory,userId:user.userId,password:user.password,sq1:user.sq1,sq2:user.sq2,sq3:user.sq3},
+    {responseType:"json"});
+  }
+}
